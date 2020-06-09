39f1a66fcb24096ac2be5896afd2601b1627b389
mrkmcode <64729649+mrkmcode@users.noreply.github.com>
2020-06-09 11:17:47 +0530

    updating our app with a shop page, a collection-preview component and a collection-item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -20,37 +20,39 @@ class Directory extends React.Component {
                   title: 'jackets',
                   imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                   id: 2,
-                  linkUrl: 'shop/jackets'
+                  linkUrl: 'jackets'
                 },
                 {
                   title: 'sneakers',
                   imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                   id: 3,
-                  linkUrl: 'shop/sneakers'
+                  linkUrl: 'sneakers'
                 },
                 {
                   title: 'womens',
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
                   id: 4,
-                  linkUrl: 'shop/womens'
+                  linkUrl: 'womens'
                 },
                 {
                   title: 'mens',
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
                   id: 5,
-                  linkUrl: 'shop/mens'
+                  linkUrl: 'mens'
                 }
             ]
         }
     }
 
     render() {
+        const { sections } = this.state;
+
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                    sections.map(({id, ...otherSectionProps}) => (
                         <MenuItem key = {id} {...otherSectionProps}></MenuItem>
                     ))
                 }
